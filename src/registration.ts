7e01a1788748741d28c03728ad52ee06eaac32ed
Роман Широков <romatallinn@gmail.com>
2018-11-20 15:12:54 +0100

    The registration date entry is removed from creation, since Firebase saves it automatically.

--- src/registration.ts
+++ src/registration.ts
@@ -16,7 +16,7 @@ exports.createEmptyUserDataInDB = (async (snapshot, context) => {
     // Obtain user's data snap in order to figure out if it's already been initialized
     // (because older versions of the IOS app initialize internally)
     // This block will be removed in the future
-    const user_db_snap = await admin.database().ref(`users/${ user_id }/date`).once("value");
+    const user_db_snap = await admin.database().ref(`users/${ user_id }/username_lowercased`).once("value");
 
     if (user_db_snap.val() !== null) {
       console.log(`The profile has already been initialized for User[${ user_id }].`);
@@ -27,16 +27,9 @@ exports.createEmptyUserDataInDB = (async (snapshot, context) => {
     console.error("ERROR CAUGHT: ", err);
   }
 
-  // Initialize date elements
-  const today = new Date();
-  const dd = today.getDate().toString();
-  const mm = (today.getMonth()+1).toString(); //January is 0!
-  const yyyy = today.getFullYear().toString();
-
   // Initialize data entry
   const data : {[key: string] : any;} = {};
   data["username_lowercased"] = original.username.toLowerCase();
-  data["date"] = `${ dd }/${ mm }/${ yyyy }`;
   data["verified"] = false;
 
   // Upload data to DB
